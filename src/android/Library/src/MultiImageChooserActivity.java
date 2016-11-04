5ab6d1d12b1eef14a8e690c2636bffead4c53d10
Maximilian Wesener <m.wesener@gmx.de>
2016-11-04 15:37:58 +0100

    Added language support for android

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -173,8 +173,8 @@ public class MultiImageChooserActivity extends Activity implements OnItemClickLi
         setupHeader();
         updateAcceptButton();
         progress = new ProgressDialog(this);
-        progress.setTitle("Processing Images");
-        progress.setMessage("This may take a few moments");
+        progress.setTitle("Bilder bearbeiten");
+        progress.setMessage("Einen Moment...");
     }
     
     @Override
@@ -189,8 +189,8 @@ public class MultiImageChooserActivity extends Activity implements OnItemClickLi
         if (maxImages == 0 && isChecked) {
             isChecked = false;
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Maximum " + maxImageCount + " Photos");
-            builder.setMessage("You can only select " + maxImageCount + " photos at a time.");
+            builder.setTitle("Maximal " + maxImageCount + " Bilder");
+            builder.setMessage("Du kannst maximal " + maxImageCount + " Bilder auswählen.");
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) { 
                     dialog.cancel();
